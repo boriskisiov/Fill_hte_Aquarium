49212555ae8564819a6ef701c3d0bad7858384bc
Boris Kisiov <137154006+boriskisiov@users.noreply.github.com>
2023-09-15 16:25:35 +0300

    Update Program.cs

--- Aquarium/Program.cs
+++ Aquarium/Program.cs
@@ -10,9 +10,10 @@
             double Percents = double.Parse(Console.ReadLine());
 
             double VolumeLiters = Lenght * Widht * Hight * 0.001;
-            double WaterPercents = (100 - Percents) / 100;
+            double WaterPercents = 1 - Percents / 100;
+            double NededWater = VolumeLiters * WaterPercents;
 
-            Console.WriteLine(VolumeLiters * WaterPercents);
+            Console.WriteLine($"(NededWater:f2)");
 
             
         }
