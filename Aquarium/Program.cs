a5dbc165eda5e3287f2095e1edd16fb68dd00df2
Boris Kisiov <137154006+boriskisiov@users.noreply.github.com>
2023-09-15 11:21:49 +0300

    Add files via upload

---
+++ Aquarium/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Aquarium
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int Lenght = int.Parse(Console.ReadLine());
+            int Widht = int.Parse(Console.ReadLine());
+            int Hight = int.Parse(Console.ReadLine());
+            double Percents = double.Parse(Console.ReadLine());
+
+            double VolumeLiters = Lenght * Widht * Hight * 0.001;
+            double WaterPercents = (100 - Percents) / 100;
+
+            Console.WriteLine(VolumeLiters * WaterPercents);
+
+            
+        }
+    }
+}
